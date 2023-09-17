a0810e1f001995ee7abd4142536eb0020d5b9e8f
PabloMuh <115596073+PabloMuh@users.noreply.github.com>
2023-09-17 20:46:59 -0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -7,61 +7,94 @@ struct Node {
     void *byte;
     int frequency;
     struct Node* next;
+    struct Node* right;
+    struct Node* left; 
 };
-
+struct Queue{
+    int current_size;
+    struct Node *head;
+};
+struct Queue *createQueue(){
+    struct Queue *New_queue = malloc(sizeof(struct Queue));
+    New_queue->head = NULL;
+    New_queue->current_size = 0;
+    return New_queue;
+}
+struct Node *createNode(void *byte,int frequency){
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->next = NULL;
+    newNode->left = NULL;
+    newNode->right = NULL;
+    newNode->byte = byte;
+    newNode->frequency = frequency;
+    return newNode;
+}
+// Cria um ponteiro para void
 void *createVoidPointer(unsigned char byte){
     unsigned char *pointer = malloc(sizeof(unsigned char*));
     *pointer = byte;
     return (void*) pointer;
 }
+// Pega o conteudo de de um ponteiro para void
 unsigned char getByteFromVoidPointer(struct Node *byte){
     unsigned char *item = (unsigned char*) byte;
     return *item;
 }
+void enqueue(struct Queue *queue,struct Node *newNode){
+    if (queue->head == NULL || newNode->frequency < queue->head->frequency){
+        newNode->next = queue->head;
+        queue->head = newNode;
+    }
+    // Aqui é para caso a inserção não ocorra no inicio
+    else{
+        struct Node* current = queue->head;
+        while (current->next != NULL && newNode->frequency >= current->next->frequency){
+            current = current->next;
+        }
+        newNode->next = current->next;
+        current->next = newNode;
+    }
+}
 /* OBJETIVO: Inserir na lista encadeada lista a frequencia e o byte
              de forma ordenada na ordem crescente.*/
-void insertionFrequency(struct Node** list, int frequency[])
+void insertionFrequency(struct Queue *queue, int frequency[])
 {
     for (int i = 0; i < 256; i++)
     {
         if (frequency[i] > 0)
         {
-            // Criar no
-            struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
-            newNode->byte = createVoidPointer(i);
-            newNode->frequency = frequency[i];
-            newNode->next = NULL;
-
-            // Se a lista estiver vazia ou o elemento for o de menor frequência aqui ocorre a inserção
-            if (*list == NULL || frequency[i] < (*list)->frequency)
-            {
-                newNode->next = *list;
-                *list = newNode;
-            }
-            // Aqui é para caso a inserção não ocorra no inicio
-            else
-            {
-                struct Node* current = *list;
-                while (current->next != NULL && frequency[i] >= current->next->frequency)
-                {
-                    current = current->next;
-                }
-                newNode->next = current->next;
-                current->next = newNode;
-            }
+            struct Node* newNode = createNode(createVoidPointer(i), frequency[i]);
+            enqueue(queue, newNode);
         }
     }
 }
 
+void print_pre_order(struct Node *bt)
+{
+    if (bt != NULL) {
+    printf("%c", getByteFromVoidPointer(bt->byte));
+    print_pre_order(bt->left);
+    print_pre_order(bt->right);
+    }
+}
+void huffmanTree(struct Queue *queue){
+    while(queue->current_size != 1){
+        int frequency = queue->head->frequency + queue->head->next->frequency;
+        struct Node *newNode = createNode(createVoidPointer('*'),frequency);
+        newNode->left = queue->head;
+        queue->head = queue->head->next;
+        newNode->right = queue->head;
+        queue->head = queue->head->next;
+        queue->current_size--;
+        enqueue(queue,newNode);
+    }
+}
 int main()
 {
     FILE *archive;
     char file_path[MAX]; // Array que armazena o nome do arquivo
     printf("Digite o caminho da pasta do arquivo: ");
     scanf("%s", file_path);
-
-    // Lista encadeada para armazenar as frequencias em ordem crescente
-    struct Node* list_frequency = NULL;
 
     // Abre o arquivo para leitura
     archive = fopen(file_path, "rb");
@@ -80,9 +113,11 @@ int main()
     {
         frequency[byte]++;
     }
-
-    insertionFrequency(&list_frequency, frequency);
-
+    //criação de uma fila de prioridade
+    struct Queue *queue = createQueue();
+    // Inserção da frequência dos elementos
+    insertionFrequency(queue, frequency);
+    
     // Exibir a tabela de frequência
     printf("Tabela de Frequencia:\n");
     for (int i = 0; i < 256; i++)
@@ -90,6 +125,7 @@ int main()
         if (frequency[i] > 0)
         {
             printf("Byte 0x%02X: %d vezes\n", i, frequency[i]);
+            queue->current_size++;
         }
     }
     
@@ -98,21 +134,21 @@ int main()
 
     // Exibir a lista encadeada
     printf("\nLista encadeada ordenada das frequencias:\n");
-    struct Node* current = list_frequency;
+    struct Node* current = queue->head;
     while (current != NULL)
     {
         printf("[%c, %d] -> ", getByteFromVoidPointer(current->byte), current->frequency);
         current = current->next;
     }
-    
     //Liberar a memoria
     struct Node* previour;
-    while (list_frequency != NULL)
+    huffmanTree(queue);
+    print_pre_order(queue->head);
+    while (queue->head != NULL)
     {
-        previour = list_frequency;
-        list_frequency = list_frequency->next;
+        previour = queue->head;
+        queue->head = queue->head->next;
         free(previour);
     }
-    
     return 0;
 }
