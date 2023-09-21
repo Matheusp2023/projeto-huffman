287a5863823435345d69a6db3491c217e3a71c48
PabloMuh <pablomunih6@gmail.com>
2023-09-21 00:34:14 -0300

    Implementção da árvore

--- inc/list.h
+++ inc/list.h
@@ -7,19 +7,27 @@ struct Node {
     void *byte;
     int frequency;
     struct Node* next;
+    struct Node *left;
+    struct Node *right;
 };
 
 typedef struct Node Node;
 
 struct Node* createList();
 
-bool isEmpaty(struct Node** list);
+bool isEmpty(struct Node** list);
+
+struct Node *createNode(void *byte,int frequency);
 
 void *createVoidPointer(unsigned char byte);
 
 unsigned char getByteFromVoidPointer(struct Node *byte);
 
-void insertionFrequency(struct Node** list, int frequency[]);
+unsigned char remove_first(struct Node **list, int *currentSize); 
+
+void addInOrder(struct Node **list,struct Node *newNode,int *currentSize);
+
+void insertionFrequency(struct Node** list, int frequency[],int *CurrentSize);
 
 void printList(struct Node* list);
 
