4c77bdb00e7e020fea9e632f53d89ee67d280646
PabloMuh <pablomunih6@gmail.com>
2023-10-07 20:08:37 -0300

    comentado

--- inc/encode.h
+++ inc/encode.h
@@ -10,19 +10,59 @@ typedef struct bithuff{
     int bitH;
     int size;
 }BitHuff;
-// Pega o tamanho do arquivo
+/**
+ * @brief Pega o tamanho de um árquivo
+ * 
+ * @param archive: O ponteiro que aponta para o Arquivo
+ * @return O tamanho do arquivo em bytes na maior variável do c unsigne long long int
+ */
 unsigned long long int sizeArchive(FILE *archive);
-// Construir a tabelo dos bits de Huffman
+/**
+ * @brief Montar a Tabela que vai conter as informações dos bit que vão representar os caracteres(bits de Huffman)
+ * 
+ * @param tree_node: A raiz da árvore de Huffman
+ * @param table: A tabela do que vai conter as informações dos bit de Huffman
+ * @param code: A variável que vai armazenar os bits de Huffman e seu tamanho até chegar em um nó folha
+ */
 void buildTable(Node* tree_node,BitHuff table[],BitHuff code);
-// Achar o tamanho do lixo
+/**
+ * @brief Retorna o tamanho do lixo no final do árquivo
+ * 
+ * @param frequency: Uma tabela com a frequência de cada caractere
+ * @param table: A tabela do que contém as informações dos bit de Huffman
+ * @return A quantidade de bits que não serão lidos no ultimo byte em inteiro
+ */
 int trashsize(int frequency[],BitHuff table[]);
-// Setar o primeiro byte que contém o tamanho do lixo e o começo do tamanho da árvore
+/**
+ * @brief Seta o primeiro byte do arquivo compactado
+ * 
+ * @param file: Um ponteiro para o arquivo compactado
+ * @param trashSize: O tamanho do lixo
+ * @param treeSize: O tamanho da árvore de Huffman
+ */
 void setFirstByte(FILE *file,int trashSize,int treeSize);
-// Setar o segundo byte que contém o restante do tamanho da árvore
+/**
+ * @brief Seta o segundo byte do arquivo compactado
+ * 
+ * @param file: Um ponteiro para o arquivo compactado
+ * @param treesize: O tamanho da árvore de Huffman
+ */
 void setSecondByte(FILE *file,int treesize);
-// Setar a árvore de huffman
+/**
+ * @brief Seta a árvore obtida em pré ordem
+ * 
+ * @param file: Um ponteiro para o arquivo compactado
+ * @param bt: O Nó raiz da Árvore de Huffman
+ */
 void setTree(FILE *file,struct Node *bt);
-// Setar os bytes com os novos bits correspondentes de cada caractere
+/**
+ * @brief Setar os bytes com os novos bits correspondentes de cada caractere
+ * 
+ * @param fileIn: Ponteiro para o árquivo original 
+ * @param fileOut: Ponteiro para o arquivo compactado
+ * @param table: A tabela do que contém as informações dos bit de Huffman
+ * @param trashSize: O tamanho do lixo
+ */
 void setBytes(FILE *fileIn,FILE *fileOut,BitHuff table[],int trashSize);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -16,17 +16,25 @@ int main() {
     scanf("%d",&i);
     if(i == 1){
         FILE *archive;
+
         char file_path[MAX]; // Array que armazena o nome do arquivo
+
         int currentSize = 0;
+
         printf("Digite o caminho da pasta do arquivo: ");
+
         scanf("%s", file_path);
         // Lista encadeada para armazenar as frequencias em ordem crescente
         struct Node* list_frequency = createList();
+
         archive = fopen(file_path, "rb");
+
         unsigned long long int size1 = sizeArchive(archive);
+
         strcat(file_path,".huff");
         // Abre o arquivo para leitura
         FILE *archiveOut = fopen(file_path,"wb");
+
         if (archiveOut == NULL)
         {
             perror("Erro ao abrir o arquivo");
@@ -39,25 +47,43 @@ int main() {
             return 1;
         } 
         int frequency[256] = {0};
+
         getFrequency(archive,frequency);
+
         insertionFrequency(&list_frequency, frequency,&currentSize);
+
         huffmanTree(&list_frequency,&currentSize);
+
         BitHuff bithuff,table[256];
+
         bithuff.bitH = 0;
+
         bithuff.size = 0;
+
         memset(table,0,sizeof(BitHuff) * 256);
+
         buildTable(list_frequency,table,bithuff);
+
         setFirstByte(archiveOut,trashsize(frequency,table),treeSize(list_frequency));
+
         setSecondByte(archiveOut,treeSize(list_frequency));
+
         setTree(archiveOut,list_frequency);
+
         fseek(archive, 0, SEEK_SET);
+
         setBytes(archive,archiveOut,table,trashsize(frequency,table));
+
         unsigned long long int size2 = sizeArchive(archiveOut);
-        deleteList(list_frequency);
+
         fclose(archive);
+
         fclose(archiveOut);
+
         printf("TAMANHO DO ARQUIVO ORIGINAL EM BYTES: %llu\n",size1);
+
         printf("TAMANHO DO ARQUIVO COMPACTADO EM BYTES: %llu\n",size2);
+
         if(size1 > size2){
             printf("A COMPRESSÃO SE DEMONSTROU EFICIENTE\n");
         }
@@ -67,22 +93,39 @@ int main() {
     }
     else if(i == 2){
         FILE *archive;
+
         FILE *archiveOut;
+
         char file_path[MAX];
+
         printf("Digite o caminho da pasta do arquivo: ");
+
         scanf("%s", file_path);
+
         archive = fopen(file_path, "rb");
+
         char *remove = strrchr(file_path,'.');
+
         if(remove != NULL) *remove = '\0';
+
         archiveOut = fopen(file_path, "wb");
+
         short int trash = trashSizeCompactFile(archive);
+
         short int treeSize = treeSizeCompactFile(archive);
+
         unsigned long long int archiveSize = sizeArchive(archive);
+
         archiveSize -= 2;
+
         archiveSize -= treeSize;
+
         Node *huffTree = getTree(archive,&treeSize);
+
         setBytesBack(archive,archiveOut,trash,archiveSize,huffTree);
+
         fclose(archive);
+        
         fclose(archiveOut);
     }
     
